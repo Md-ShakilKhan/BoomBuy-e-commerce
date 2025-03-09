5a3ff96ef95c9e596d8fbe3e2718dd8cd04a13d6
Md-ShakilKhan <mdshakil.khan8032@gmail.com>
2025-03-10 03:41:41 +0600

    Category wise search functionalities generated

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -5,7 +5,7 @@ import ProductThumb from "./ProductThumb";
 
 function ProductGrid({ products }: { products: Product[] }) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ga-4 mt-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4 p-4">
       {products?.map((product) => {
         return (
           <AnimatePresence key={product._id}>
